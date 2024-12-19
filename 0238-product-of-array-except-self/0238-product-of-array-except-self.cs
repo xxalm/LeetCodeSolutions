9b62b8b751f4737b42e7566365c8e65d5d04216f
Leckson Almeida <leckalmeida@gmail.com>
2024-12-19 17:14:22 -0300

    Time: 1 ms (100%), Space: 66.2 MB (42.73%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] answer = new int[n];
+
+        
+        answer[0] = 1; 
+        for (int i = 1; i < n; i++)
+        {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            answer[i] = answer[i] * suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
+    }
+}
