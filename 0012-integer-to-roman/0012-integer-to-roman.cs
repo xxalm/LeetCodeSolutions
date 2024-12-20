1a0b8e6c0bf68e27d892881a25c5c343368a2e40
Leckson Almeida <leckalmeida@gmail.com>
2024-12-20 13:11:22 -0300

    Time: 3 ms (68.2%), Space: 46.6 MB (36.04%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        StringBuilder roman = new StringBuilder();
+
+        
+        for (int i = 0; i < values.Length; i++)
+        {
+            while (num >= values[i])
+            {
+                roman.Append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return roman.ToString();
+    }
+}
