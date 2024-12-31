948dddf3a262d36cba713d8cc462e664aa434a9a
Leckson Almeida <leckalmeida@gmail.com>
2024-12-31 09:01:29 -0300

    Time: 1 ms (73.46%), Space: 49.1 MB (36.63%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+        foreach (int num in nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
