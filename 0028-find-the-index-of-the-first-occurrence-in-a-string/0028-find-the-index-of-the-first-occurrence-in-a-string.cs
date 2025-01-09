462971b5ee09f6ecd3dbfcdbae0ae5443ce59b6e
Leckson Almeida <leckalmeida@gmail.com>
2025-01-08 23:40:24 -0300

    Time: 10 ms (9.11%), Space: 40.4 MB (6.34%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -0,0 +1,11 @@
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+
+        if (string.IsNullOrEmpty(needle))
+            return 0;
+
+        return haystack.IndexOf(needle);
+    }
+}
